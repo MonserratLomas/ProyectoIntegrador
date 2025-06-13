163aa06cdc58430f370365d128c681a556ec133f
MonserratLomas <A01384654@tec.mx>
2025-06-13 17:41:01 -0600

    Create Episodio.h

---
+++ Episodio.h
@@ -0,0 +1,23 @@
+#ifndef EPISODIO_H
+#define EPISODIO_H
+
+#include <string>
+#include <vector>
+#include <numeric>
+
+class Episodio {
+private:
+    std::string titulo;
+    int temporada;
+    std::vector<int> calificaciones;
+
+public:
+    Episodio(const std::string& titulo, int temporada);
+
+    std::string getTitulo() const;
+    int getTemporada() const;
+    void calificar(int valor);
+    double obtenerCalificacionPromedio() const;
+};
+
+#endif
