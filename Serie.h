f5c6481a3d9a561d99cb8e77ae734d8012296cba
MonserratLomas <A01384654@tec.mx>
2025-06-13 17:42:29 -0600

    Create Serie.h

---
+++ Serie.h
@@ -0,0 +1,16 @@
+#ifndef SERIE_H
+#define SERIE_H
+
+#include "Video.h"
+
+class Serie : public Video {
+private:
+    int temporada;
+    std::string tituloEpisodio;
+
+public:
+    Serie(const std::string& id, const std::string& nombre, int duracion, const std::string& genero, int temporada, const std::string& tituloEpisodio);
+
+    int getTemporada() const;
+    std::string getTituloEpisodio() const;
+};
