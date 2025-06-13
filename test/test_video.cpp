6954401bf8003fa2f2964aa5c46832f024cd94f7
MonserratLomas <A01384654@tec.mx>
2025-06-13 17:46:27 -0600

    Update and rename test_divide.cpp to test_video.cpp

---
+++ test/test_video.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+#include "../Video.h"
+
+TEST(VideoTest, CreaVideoCorrectamente) {
+    Video video("001", "Matrix", 120, "Acción");
+
+    EXPECT_EQ(video.getId(), "001");
+    EXPECT_EQ(video.getNombre(), "Matrix");
+    EXPECT_EQ(video.getDuracion(), 120);
+    EXPECT_EQ(video.getGenero(), "Acción");
+}
+
+TEST(VideoTest, CalificacionVaciaDevuelveCero) {
+    Video video("002", "Vacío", 90, "Drama");
+
+    EXPECT_DOUBLE_EQ(video.getCalificacion(), 0.0);
+}
+
+TEST(VideoTest, AgregaCalificacionesValidas) {
+    Video video("003", "Test", 100, "Comedia");
+
+    video.agregarCalificacion(4);
+    video.agregarCalificacion(5);
+
+    EXPECT_DOUBLE_EQ(video.getCalificacion(), 4.5);
+}
+
+TEST(VideoTest, RechazaCalificacionesInvalidas) {
+    Video video("004", "Test", 100, "Comedia");
+
+    EXPECT_THROW(video.agregarCalificacion(0), std::out_of_range);
+    EXPECT_THROW(video.agregarCalificacion(6), std::out_of_range);
+}
+
+TEST(VideoTest, LanzaExcepcionDuracionNegativa) {
+    EXPECT_THROW(Video("005", "Error", -10, "Terror"), std::invalid_argument);
+}
