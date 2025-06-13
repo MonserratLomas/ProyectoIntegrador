80e4bf7174d26346c2cd6d8e76faeb27fd371794
MonserratLomas <A01384654@tec.mx>
2025-06-13 17:53:36 -0600

    Create Episodio.cpp

---
+++ Episodio.cpp
@@ -0,0 +1,17 @@
+#include "Episodio.h"
+
+Episodio::Episodio(const std::string& titulo, int temporada)
+    : titulo(titulo), temporada(temporada) {}
+
+std::string Episodio::getTitulo() const { return titulo; }
+int Episodio::getTemporada() const { return temporada; }
+
+void Episodio::calificar(int valor) {
+    if (valor >= 1 && valor <= 5)
+        calificaciones.push_back(valor);
+}
+
+double Episodio::obtenerCalificacionPromedio() const {
+    if (calificaciones.empty()) return 0.0;
+    return static_cast<double>(std::accumulate(calificaciones.begin(), calificaciones.end(), 0)) / calificaciones.size();
+}
