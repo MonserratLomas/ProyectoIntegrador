26ade54650b3b3c4943f6f17f03e176bcd0471a6
MonserratLomas <A01384654@tec.mx>
2025-06-13 17:46:57 -0600

    Create Video.cpp

---
+++ Video.cpp
@@ -0,0 +1,19 @@
+#include "Video.h"
+
+Video::Video(const std::string& id, const std::string& nombre, int duracion, const std::string& genero)
+    : id(id), nombre(nombre), duracion(duracion), genero(genero) {}
+
+std::string Video::getId() const { return id; }
+std::string Video::getNombre() const { return nombre; }
+int Video::getDuracion() const { return duracion; }
+std::string Video::getGenero() const { return genero; }
+
+void Video::agregarCalificacion(int cal) {
+    if (cal >= 1 && cal <= 5)
+        calificaciones.push_back(cal);
+}
+
+double Video::getCalificacion() const {
+    if (calificaciones.empty()) return 0.0;
+    return static_cast<double>(std::accumulate(calificaciones.begin(), calificaciones.end(), 0)) / calificaciones.size();
+}
