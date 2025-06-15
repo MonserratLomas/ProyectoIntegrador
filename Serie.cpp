ab2f3ef0837aca310a92857431e898bfea45fd48
MonserratLomas <A01384654@tec.mx>
2025-06-14 23:41:55 -0600

    Update Serie.cpp

--- Serie.cpp
+++ Serie.cpp
@@ -1,3 +1,12 @@
+/*
+ * Serie.cpp
+ * 
+ * Implementación de la clase Serie, que hereda de Video y contiene episodios.
+ * 
+ * Proyecto: Proyecto Integrador - Programación Orientada a Objetos
+ * Autor: Monserrat Hernández Lomas
+ * Fecha: Junio 2025
+ */
 #include "Serie.h"
 
 Serie::Serie(const std::string& id, const std::string& nombre, int duracion, const std::string& genero, int temporada, const std::string& tituloEpisodio)
