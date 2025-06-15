6bc2c247ce2f017233fd0d94749ae72e697846cf
MonserratLomas <A01384654@tec.mx>
2025-06-15 00:19:24 -0600

    Update test_serie.cpp

--- test/test_serie.cpp
+++ test/test_serie.cpp
@@ -13,28 +13,28 @@
 TEST(SerieTest, CreaSerieCorrectamente) {
     Serie serie("010", "Breaking Bad", 45, "Drama", 1, "Pilot");
 
-    EXPECT_EQ(serie.getId(), "010");
-    EXPECT_EQ(serie.getNombre(), "Breaking Bad");
-    EXPECT_EQ(serie.getDuracion(), 45);
-    EXPECT_EQ(serie.getGenero(), "Drama");
-    EXPECT_EQ(serie.getTemporada(), 1);
-    EXPECT_EQ(serie.getTituloEpisodio(), "Pilot");
+    EXPECT_EQ(serie.GetId(), "010");
+    EXPECT_EQ(serie.GetNombre(), "Breaking Bad");
+    EXPECT_EQ(serie.GetDuracion(), 45);
+    EXPECT_EQ(serie.GetGenero(), "Drama");
+    EXPECT_EQ(serie.GetTemporada(), 1);
+    EXPECT_EQ(serie.GetTituloEpisodio(), "Pilot");
 }
 
 TEST(SerieTest, HeredaCalificacionDeVideo) {
     Serie serie("011", "Stranger Things", 50, "Ciencia Ficción", 2, "Chapter One");
 
-    EXPECT_DOUBLE_EQ(serie.getCalificacion(), 0.0);
+    EXPECT_DOUBLE_EQ(serie.GetCalificacion(), 0.0);
 
-    serie.agregarCalificacion(3);
-    serie.agregarCalificacion(4);
+    serie.AgregarCalificacion(3);
+    serie.AgregarCalificacion(4);
 
-    EXPECT_DOUBLE_EQ(serie.getCalificacion(), 3.5);
+    EXPECT_DOUBLE_EQ(serie.GetCalificacion(), 3.5);
 }
 
 TEST(SerieTest, RechazaCalificacionesInvalidas) {
     Serie serie("012", "The Witcher", 55, "Fantasía", 3, "The End's Beginning");
 
-    EXPECT_THROW(serie.agregarCalificacion(0), std::out_of_range);
-    EXPECT_THROW(serie.agregarCalificacion(6), std::out_of_range);
+    EXPECT_THROW(serie.AgregarCalificacion(0), std::out_of_range);
+    EXPECT_THROW(serie.AgregarCalificacion(6), std::out_of_range);
 }
