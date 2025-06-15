c87051f56dd51bc2e85872d40b986b00d7d3721d
MonserratLomas <A01384654@tec.mx>
2025-06-14 22:54:28 -0600

    Update Episodio.cpp

--- Episodio.cpp
+++ Episodio.cpp
@@ -1,4 +1,6 @@
 #include "Episodio.h"
+#include <stdexcept>
+#include <numeric>
 
 Episodio::Episodio(const std::string& titulo, int temporada)
     : titulo(titulo), temporada(temporada) {}
@@ -7,8 +9,10 @@ std::string Episodio::getTitulo() const { return titulo; }
 int Episodio::getTemporada() const { return temporada; }
 
 void Episodio::calificar(int valor) {
-    if (valor >= 1 && valor <= 5)
-        calificaciones.push_back(valor);
+    if (valor < 1 || valor > 5) {
+        throw std::out_of_range("Calificación fuera de rango (debe estar entre 1 y 5)");
+    }
+    calificaciones.push_back(valor);
 }
 
 double Episodio::obtenerCalificacionPromedio() const {
