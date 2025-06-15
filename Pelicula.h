b93645fbc4da5de4e314d799543060b3d445a0db
MonserratLomas <A01384654@tec.mx>
2025-06-14 23:52:05 -0600

    Update Pelicula.h

--- Pelicula.h
+++ Pelicula.h
@@ -7,13 +7,27 @@
  * Autor: Monserrat Hernández Lomas
  * Fecha: Junio 2025
  */
+
 #ifndef PELICULA_H
 #define PELICULA_H
 
 #include "Video.h"
 
+/**
+ * @class Pelicula
+ * @brief Clase que representa una película, hereda de Video.
+ * 
+ * Implementa características específicas para películas.
+ */
 class Pelicula : public Video {
 public:
+    /**
+     * @brief Constructor de la clase Pelicula.
+     * @param id Identificador único de la película.
+     * @param nombre Nombre o título de la película.
+     * @param duracion Duración de la película en minutos.
+     * @param genero Género de la película.
+     */
     Pelicula(const std::string& id, const std::string& nombre, int duracion, const std::string& genero);
 };
 
