ec53542407a1b12b43caae9dbdc67f959b9f5d0a
MonserratLomas <A01384654@tec.mx>
2025-06-14 23:41:43 -0600

    Update Pelicula.cpp

--- Pelicula.cpp
+++ Pelicula.cpp
@@ -1,3 +1,12 @@
+/*
+ * Pelicula.cpp
+ * 
+ * Implementación de la clase Pelicula, que hereda de Video y añade duración.
+ * 
+ * Proyecto: Proyecto Integrador - Programación Orientada a Objetos
+ * Autor: Monserrat Hernández Lomas
+ * Fecha: Junio 2025
+ */
 #include "Pelicula.h"
 
 Pelicula::Pelicula(const std::string& id, const std::string& nombre, int duracion, const std::string& genero)
