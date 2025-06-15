6052579aef412275192fc1ad952c6a0029d63c60
MonserratLomas <A01384654@tec.mx>
2025-06-14 23:42:39 -0600

    Update Pelicula.h

--- Pelicula.h
+++ Pelicula.h
@@ -1,3 +1,12 @@
+/*
+ * Pelicula.h
+ * 
+ * Declaración de la clase Pelicula que hereda de Video.
+ * 
+ * Proyecto: Proyecto Integrador - Programación Orientada a Objetos
+ * Autor: Monserrat Hernández Lomas
+ * Fecha: Junio 2025
+ */
 #ifndef PELICULA_H
 #define PELICULA_H
 
