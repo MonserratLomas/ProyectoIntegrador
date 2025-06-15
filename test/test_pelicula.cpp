603f9f08223dc3884c49814fae39c45e50842c7e
MonserratLomas <A01384654@tec.mx>
2025-06-15 00:18:55 -0600

    Update test_pelicula.cpp

--- test/test_pelicula.cpp
+++ test/test_pelicula.cpp
@@ -13,30 +13,30 @@
 TEST(PeliculaTest, CreaPeliculaCorrectamente) {
     Pelicula pelicula("100", "Inception", 148, "Ciencia Ficción");
 
-    EXPECT_EQ(pelicula.getId(), "100");
-    EXPECT_EQ(pelicula.getNombre(), "Inception");
-    EXPECT_EQ(pelicula.getDuracion(), 148);
-    EXPECT_EQ(pelicula.getGenero(), "Ciencia Ficción");
+    EXPECT_EQ(pelicula.GetId(), "100");
+    EXPECT_EQ(pelicula.GetNombre(), "Inception");
+    EXPECT_EQ(pelicula.GetDuracion(), 148);
+    EXPECT_EQ(pelicula.GetGenero(), "Ciencia Ficción");
 }
 
 TEST(PeliculaTest, CalificacionInicialEsCero) {
     Pelicula pelicula("101", "Interstellar", 169, "Ciencia Ficción");
 
-    EXPECT_DOUBLE_EQ(pelicula.getCalificacion(), 0.0);
+    EXPECT_DOUBLE_EQ(pelicula.GetCalificacion(), 0.0);
 }
 
 TEST(PeliculaTest, AgregaCalificacionesYCalculaPromedio) {
     Pelicula pelicula("102", "The Matrix", 136, "Acción");
 
-    pelicula.agregarCalificacion(5);
-    pelicula.agregarCalificacion(4);
+    pelicula.AgregarCalificacion(5);
+    pelicula.AgregarCalificacion(4);
 
-    EXPECT_DOUBLE_EQ(pelicula.getCalificacion(), 4.5);
+    EXPECT_DOUBLE_EQ(pelicula.GetCalificacion(), 4.5);
 }
 
 TEST(PeliculaTest, RechazaCalificacionesInvalidas) {
     Pelicula pelicula("103", "Gladiator", 155, "Acción");
 
-    EXPECT_THROW(pelicula.agregarCalificacion(0), std::out_of_range);
-    EXPECT_THROW(pelicula.agregarCalificacion(6), std::out_of_range);
+    EXPECT_THROW(pelicula.AgregarCalificacion(0), std::out_of_range);
+    EXPECT_THROW(pelicula.AgregarCalificacion(6), std::out_of_range);
 }
