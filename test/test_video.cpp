8482306a213963e630619c191d5bef28ba22b1b7
MonserratLomas <A01384654@tec.mx>
2025-06-14 23:43:36 -0600

    Update test_video.cpp

--- test/test_video.cpp
+++ test/test_video.cpp
@@ -1,3 +1,12 @@
+/*
+ * test_video.cpp
+ * 
+ * Pruebas unitarias para la clase Video.
+ * 
+ * Proyecto: Proyecto Integrador - Programación Orientada a Objetos
+ * Autor: Monserrat Hernández Lomas
+ * Fecha: Junio 2025
+ */
 #include <gtest/gtest.h>
 #include "../Video.h"
 
