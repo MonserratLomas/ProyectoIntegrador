8b20585c207f3b3c6319c0190021c72c77ea415f
MonserratLomas <A01384654@tec.mx>
2025-06-14 23:52:46 -0600

    Update Serie.h

--- Serie.h
+++ Serie.h
@@ -7,21 +7,46 @@
  * Autor: Monserrat Hernández Lomas
  * Fecha: Junio 2025
  */
+
 #ifndef SERIE_H
 #define SERIE_H
 
 #include "Video.h"
 #include <string>
 
+/**
+ * @class Serie
+ * @brief Clase que representa una serie de televisión o similar, hereda de Video.
+ * 
+ * Contiene información adicional específica como temporada y título del episodio.
+ */
 class Serie : public Video {
 private:
-    int temporada;
-    std::string tituloEpisodio;
+    int temporada;               ///< Número de la temporada
+    std::string tituloEpisodio; ///< Título del episodio
 
 public:
+    /**
+     * @brief Constructor de la clase Serie.
+     * @param id Identificador único de la serie.
+     * @param nombre Nombre de la serie.
+     * @param duracion Duración del episodio en minutos.
+     * @param genero Género de la serie.
+     * @param temporada Número de la temporada del episodio.
+     * @param tituloEpisodio Título del episodio.
+     */
     Serie(const std::string& id, const std::string& nombre, int duracion, const std::string& genero, int temporada, const std::string& tituloEpisodio);
 
+    /**
+     * @brief Obtiene el número de temporada del episodio.
+     * @return Número de temporada.
+     */
     int getTemporada() const;
+
+    /**
+     * @brief Obtiene el título del episodio.
+     * @return Título del episodio.
+     */
     std::string getTituloEpisodio() const;
 };
 
