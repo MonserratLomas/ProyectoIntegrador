e2accd804706a28725b2c033563a9f8ed3f52482
MonserratLomas <A01384654@tec.mx>
2025-06-14 23:43:21 -0600

    Update test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,3 +1,12 @@
+/*
+ * test_main.cpp
+ * 
+ * Archivo principal de pruebas unitarias que ejecuta todos los tests con Google Test.
+ * 
+ * Proyecto: Proyecto Integrador - Programación Orientada a Objetos
+ * Autor: Monserrat Hernández Lomas
+ * Fecha: Junio 2025
+ */
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
